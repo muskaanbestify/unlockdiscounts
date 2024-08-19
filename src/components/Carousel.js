a614131ddddc8b93bc367d3811125e0e3660da69
ogopi0 <gopivenkatcool@gmail.com>
2024-08-19 17:46:48 +0530

    testing message

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,18 @@
-
-import React, { useState, useEffect } from 'react';
-import './carousel.css';
+import React, { useState, useEffect } from "react";
+import "./carousel.css";
 
 function Carousel() {
   // State to keep track of the active slide index
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // Array of slide image URLs
+  // Array of slide image URLs.
   const slides = [
     "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328894/slide-1_w5npro.jpg",
     "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328933/slide-2_ibse0k.jpg",
     "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328226/slide_3_peumsu.png",
     "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328226/slide_4_shabup.png",
-    "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328226/slide_5_egri89.png"
-  ];  //Used Cloudinary for creating a virtual link for those images
+    "https://res.cloudinary.com/dcmtxvsav/image/upload/v1722328226/slide_5_egri89.png",
+  ]; //Used Cloudinary for creating a virtual link for those images
 
   // Function to handle moving to the next slide
   const nextSlide = () => {
@@ -23,10 +22,10 @@ function Carousel() {
   // Preload images
   useEffect(() => {
     slides.forEach((slide) => {
-      const link = document.createElement('link');
-      link.rel = 'preload';
+      const link = document.createElement("link");
+      link.rel = "preload";
       link.href = slide;
-      link.as = 'image';
+      link.as = "image";
       document.head.appendChild(link);
     });
   }, [slides]);
@@ -40,8 +39,14 @@ function Carousel() {
   return (
     <div className="carousel">
       {slides.map((slide, index) => (
-        <div key={index} className={index === activeIndex ? "carousel-item active" : "carousel-item"}>
-          <img loading='lazy' src={slide} alt={`Slide ${index + 1}`} />  {/* Added Loading Lazy */ }
+        <div
+          key={index}
+          className={
+            index === activeIndex ? "carousel-item active" : "carousel-item"
+          }
+        >
+          <img loading="lazy" src={slide} alt={`Slide ${index + 1}`} />{" "}
+          {/* Added Loading Lazy */}
         </div>
       ))}
     </div>
@@ -49,4 +54,3 @@ function Carousel() {
 }
 
 export default Carousel;
-
